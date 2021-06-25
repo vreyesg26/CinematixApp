4d813af5a8f7d65668966ad663a17a9aafe36308
CesarMolina9 <73322252+CesarMolina9@users.noreply.github.com>
2021-06-25 02:19:20 -0600

    Pruebas unitarias avance 2

--- CinematixApp/test/JFrames/ControlSalasTest.java
+++ CinematixApp/test/JFrames/ControlSalasTest.java
@@ -39,111 +39,151 @@ public class ControlSalasTest {
         ControlSalas instance = new ControlSalas();
         Image expResult = null;
         Image result = instance.getIconImage();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        if (result == null) {
+            fail("The test case is a prototype.");
+        }
     }
 
     @Test
     public void testDisponibles2D() {
         System.out.println("disponibles2D");
-        ControlSalas instance = new ControlSalas();
-        instance.disponibles2D();
-        fail("The test case is a prototype.");
+        ControlSalas instance = new ControlSalas(); 
+        try {
+            instance.disponibles2D();
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }   
     }
 
     @Test
     public void testReservados2D() {
         System.out.println("reservados2D");
         ControlSalas instance = new ControlSalas();
-        instance.reservados2D();
-        fail("The test case is a prototype.");
+        try {
+            instance.reservados2D();
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }
     }
 
     @Test
     public void testDisponibles3D() {
         System.out.println("disponibles3D");
         ControlSalas instance = new ControlSalas();
-        instance.disponibles3D();
-        fail("The test case is a prototype.");
+        try {
+            instance.disponibles3D();
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }
     }
 
     @Test
     public void testReservados3D() {
         System.out.println("reservados3D");
         ControlSalas instance = new ControlSalas();
-        instance.reservados3D();
-        fail("The test case is a prototype.");
+        try {
+            instance.reservados3D();
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }
     }
 
     @Test
     public void testDisponiblesMax2D() {
         System.out.println("disponiblesMax2D");
-        ControlSalas instance = new ControlSalas();
-        instance.disponiblesMax2D();
-        fail("The test case is a prototype.");
+        ControlSalas instance = new ControlSalas(); 
+        try {
+            instance.disponiblesMax2D();
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }
     }
 
     @Test
     public void testReservadosMax2D() {
         System.out.println("reservadosMax2D");
         ControlSalas instance = new ControlSalas();
-        instance.reservadosMax2D();
-        fail("The test case is a prototype.");
+        try {
+            instance.reservadosMax2D();
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }
     }
 
     @Test
     public void testDisponiblesMax3D() {
         System.out.println("disponiblesMax3D");
         ControlSalas instance = new ControlSalas();
-        instance.disponiblesMax3D();
-        fail("The test case is a prototype.");
+        try {
+            instance.disponiblesMax3D();
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }
     }
 
     @Test
     public void testReservadosMax3D() {
         System.out.println("reservadosMax3D");
         ControlSalas instance = new ControlSalas();
-        instance.reservadosMax3D();
-        fail("The test case is a prototype.");
+        try {
+            instance.reservadosMax3D();
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }
     }
 
     @Test
     public void testEstadoSala2D() {
         System.out.println("estadoSala2D");
         ControlSalas instance = new ControlSalas();
-        instance.estadoSala2D();
-        fail("The test case is a prototype.");
+        try {
+            instance.estadoSala2D();
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }
     }
 
     @Test
     public void testEstadoSala3D() {
         System.out.println("estadoSala3D");
         ControlSalas instance = new ControlSalas();
-        instance.estadoSala3D();
-        fail("The test case is a prototype.");
+        try {
+            instance.estadoSala3D();
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }
     }
 
     @Test
     public void testEstadoSalaMax2D() {
         System.out.println("estadoSalaMax2D");
         ControlSalas instance = new ControlSalas();
-        instance.estadoSalaMax2D();
-        fail("The test case is a prototype.");
+        try {
+            instance.estadoSalaMax2D();
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }
     }
 
     @Test
     public void testEstadoSalaMax3D() {
         System.out.println("estadoSalaMax3D");
         ControlSalas instance = new ControlSalas();
-        instance.estadoSalaMax3D();
-        fail("The test case is a prototype.");
+        try {
+            instance.estadoSalaMax3D();
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }
     }
 
     @Test
     public void testMain() {
         System.out.println("main");
         String[] args = null;
-        ControlSalas.main(args);
-        fail("The test case is a prototype.");
+        try {
+            ControlSalas.main(args);
+        } catch(Exception e){
+            fail("The test case is a prototype.");
+        }
     }
 }
